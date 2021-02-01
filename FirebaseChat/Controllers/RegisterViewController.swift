aa9fb7d250195b4be30221d95c0d6d7ca829f360
IvanPrasetyoNugroho <ivanprasetyo559@gmail.com>
2021-02-01 21:53:05 +0700

    Setup Login and Register View Controller

--- FirebaseChat/Controllers/RegisterViewController.swift
+++ FirebaseChat/Controllers/RegisterViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "messenger")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .link
+        
+        // Add subview
+        view.addSubview(imageView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.frame.size.width / 3
+        imageView.frame = CGRect(x: (view.frame.size.width - size) / 2, y: 20, width: size, height: size)
     }
-    */
 
 }
