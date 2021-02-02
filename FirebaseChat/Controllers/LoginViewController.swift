9e3c0add5f1104e44572e066976a57a64de51b05
IvanPrasetyoNugroho <ivanprasetyo559@gmail.com>
2021-02-02 21:13:54 +0700

    Setting Up Views for Login Screen

--- FirebaseChat/Controllers/LoginViewController.swift
+++ FirebaseChat/Controllers/LoginViewController.swift
@@ -8,14 +8,80 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "messenger")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.returnKeyType = .continue
+        textField.layer.cornerRadius = 10
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.placeholder = "Email Address"
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        textField.leftViewMode = .always
+        textField.backgroundColor = .clear
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.returnKeyType = .done
+        textField.layer.cornerRadius = 10
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.placeholder = "Password"
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        textField.leftViewMode = .always
+        textField.backgroundColor = .clear
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Log In"
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
+        
+        loginButton.addTarget(self, action: #selector(didTapLoginButton), for: .touchUpInside)
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        // Add subview
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailTextField)
+        scrollView.addSubview(passwordTextField)
+        scrollView.addSubview(loginButton)
     }
     
     @objc private func didTapRegister() {
@@ -24,5 +90,43 @@ class LoginViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
     
+    @objc private func didTapLoginButton() {
+        guard let email = emailTextField.text, let password = passwordTextField.text, !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+            alertUser()
+            return
+        }
+    }
     
+    private func alertUser() {
+        let alert = UIAlertController(title: "Whoops!", message: "Please enter all information to continue", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        
+        let size = scrollView.width / 3
+        imageView.frame = CGRect(x: (scrollView.width - size) / 2, y: 20, width: size, height: size)
+        
+        emailTextField.frame = CGRect(x: 30, y:imageView.bottom + 10, width: scrollView.width - 60, height: 52)
+        
+        passwordTextField.frame = CGRect(x: 30, y:emailTextField.bottom + 10, width: scrollView.width - 60, height: 52)
+        
+        loginButton.frame = CGRect(x: 30, y:passwordTextField.bottom + 10, width: scrollView.width - 60, height: 52)
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            didTapLoginButton()
+        }
+        
+        return true
+    }
 }
--- FirebaseChat/Controllers/RegisterViewController.swift
+++ FirebaseChat/Controllers/RegisterViewController.swift
@@ -9,25 +9,10 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
-    private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "messenger")
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .link
         
-        // Add subview
-        view.addSubview(imageView)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        let size = view.frame.size.width / 3
-        imageView.frame = CGRect(x: (view.frame.size.width - size) / 2, y: 20, width: size, height: size)
     }
 
 }
