5d4b9f68e154f92c438b7616a726e358ea699c2f
IvanPrasetyoNugroho <ivanprasetyo559@gmail.com>
2021-02-03 22:07:57 +0700

    Set Image Picker to Registration screen to update the profile picture

--- FirebaseChat/Controllers/RegisterViewController.swift
+++ FirebaseChat/Controllers/RegisterViewController.swift
@@ -13,6 +13,9 @@ class RegisterViewController: UIViewController {
         imageView.image = UIImage(systemName: "person")
         imageView.tintColor = .gray
         imageView.contentMode = .scaleAspectFit
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 2
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
         return imageView
     }()
     
@@ -83,7 +86,7 @@ class RegisterViewController: UIViewController {
         return textField
     }()
     
-    private let loginButton: UIButton = {
+    private let registerButton: UIButton = {
         let button = UIButton()
         button.setTitle("Log In", for: .normal)
         button.backgroundColor = .link
@@ -100,7 +103,7 @@ class RegisterViewController: UIViewController {
         title = "Log In"
         view.backgroundColor = .white
         
-        loginButton.addTarget(self, action: #selector(didTapRegisterButton), for: .touchUpInside)
+        registerButton.addTarget(self, action: #selector(didTapRegisterButton), for: .touchUpInside)
         
         firstNameField.delegate = self
         lastNameField.delegate = self
@@ -114,7 +117,7 @@ class RegisterViewController: UIViewController {
         scrollView.addSubview(lastNameField)
         scrollView.addSubview(emailTextField)
         scrollView.addSubview(passwordTextField)
-        scrollView.addSubview(loginButton)
+        scrollView.addSubview(registerButton)
         
         // Enable user interaction
         scrollView.isUserInteractionEnabled = true
@@ -126,7 +129,7 @@ class RegisterViewController: UIViewController {
     }
     
     @objc private func didTapProfilePic() {
-        print("Profile Pic got tapped")
+        profilePictureOption()
     }
     
     @objc private func didTapRegisterButton() {
@@ -162,6 +165,7 @@ class RegisterViewController: UIViewController {
         
         let size = scrollView.width / 3
         imageView.frame = CGRect(x: (scrollView.width - size) / 2, y: 20, width: size, height: size)
+        imageView.layer.cornerRadius = imageView.width / 2
         
         firstNameField.frame = CGRect(x: 30, y:imageView.bottom + 10, width: scrollView.width - 60, height: 52)
         
@@ -171,7 +175,7 @@ class RegisterViewController: UIViewController {
         
         passwordTextField.frame = CGRect(x: 30, y:emailTextField.bottom + 10, width: scrollView.width - 60, height: 52)
         
-        loginButton.frame = CGRect(x: 30, y:passwordTextField.bottom + 10, width: scrollView.width - 60, height: 52)
+        registerButton.frame = CGRect(x: 30, y:passwordTextField.bottom + 10, width: scrollView.width - 60, height: 52)
     }
 }
 
@@ -189,4 +193,59 @@ extension RegisterViewController: UITextFieldDelegate {
         
         return true
     }
+}
+
+extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func profilePictureOption() {
+        let optionAlert = UIAlertController(title: "Please select the option", message: nil, preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { [weak self] (UIAlertAction) in
+            self?.profilePictureFromCamera()
+        }
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { [weak self] (UIAlertAction) in
+            self?.profilePictureFromPhotoLibrary()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        optionAlert.addAction(cameraAction)
+        optionAlert.addAction(photoLibraryAction)
+        optionAlert.addAction(cancelAction)
+        
+        present(optionAlert, animated: true, completion: nil)
+    }
+    
+    func profilePictureFromCamera() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func profilePictureFromPhotoLibrary() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true, completion: nil)
+        
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        
+        imageView.image = selectedImage
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
